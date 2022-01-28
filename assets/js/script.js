a682c4891117232bf6b25187609f1ab59d4a4e5a
mjeammet <marie.jeammet@protonmail.com>
2022-01-28 02:21:09 +0100

    comment the stupid alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const currentbook_ISBN = "9780099582151"
-// Pardonnez cette infame mais pas de db = marie perdue
+// Pardonnez cette infamie mais pas de db = marie perdue
 
-load_currentbook();
+// load_currentbook();
 
 function load_currentbook(){
     alert("function correctement appelée");
