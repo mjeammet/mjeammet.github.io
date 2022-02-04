bf52bc8bf3bca78a61914a6991fd86b5fd62e6b5
mjeammet <marie.jeammet@protonmail.com>
2022-02-04 19:56:14 +0100

    turning bland html into crazy javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,48 @@
-const currentbook_ISBN = "9782070336197";
-// Pardonnez cette infamie mais pas sans db, voilà commment ça se finit. json incoming ?
-const bookapi_url = `https://openlibrary.org/api/books?bibkeys=ISBN:${currentbook_ISBN}&jscmd=data&format=json`;
+var jsonPath = "./projects.json";
+jsonData = loadJson(jsonPath);
 
-load_currentbook();
+currentBookISBN = jsonData['current_book']['isbn'];
+load_currentbook(currentBookISBN);
 
-async function load_currentbook(){
+projects = jsonData["projects"];
+load_projects(projects);
+
+
+function loadJson(jsonPath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", jsonPath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return JSON.parse(result);
+}
+
+/* 
+Load current book infos from openlibrary.org 
+and modify index.html to display it in the dedicate section
+*/
+async function load_currentbook(isbn){
+    let bookapi_url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`;
     let api_response = await fetch(bookapi_url);
     let data_json = await api_response.json();
-    let isbn_object = data_json[`ISBN:${currentbook_ISBN}`]
-    console.log(isbn_object);
-
-    cover_url = isbn_object.cover.medium;
+    let isbn_object = data_json[`ISBN:${isbn}`]
+    // console.log(isbn_object);
 
     let cover = document.getElementById('currentbook_cover');
     cover.href = isbn_object.url
     let cover_img = cover.childNodes[0];
+    cover_url = isbn_object.cover.medium;
     cover_img.src = cover_url
 
     let basic_infos = document.getElementById('currentbook_basicinfo');
     basic_infos.innerHTML = `${isbn_object.title}<br>by ${isbn_object.authors[0].name}`;
 }
- 
+
+function load_projects(projects_list){
+
+    for (project of projects_list){
+        alert(project);
+    } 
+} 
