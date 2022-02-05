48794a1c2f057225ec2aac339c116eb2d5f05170
mjeammet <marie.jeammet@protonmail.com>
2022-02-05 02:00:19 +0100

    loading projects using javascript and json

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var jsonPath = "./projects.json";
+var jsonPath = "./data.json";
 jsonData = loadJson(jsonPath);
 
 currentBookISBN = jsonData['current_book']['isbn'];
@@ -19,10 +19,11 @@ function loadJson(jsonPath) {
     return JSON.parse(result);
 }
 
-/* 
-Load current book infos from openlibrary.org 
-and modify index.html to display it in the dedicate section
-*/
+/**
+ * Load current book infos from openlibrary.org 
+ * and modify index.html to display it in the dedicate section
+ * @param isbn book isbn-13 (starting with '9 78')
+ */
 async function load_currentbook(isbn){
     let bookapi_url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`;
     let api_response = await fetch(bookapi_url);
@@ -44,5 +45,40 @@ function load_projects(projects_list){
 
     for (project of projects_list){
         console.log(project);
+        new_project = buildProjectBox(project);
+        document.getElementById("projects_box").appendChild(new_project);
     } 
 } 
+
+function buildProjectBox(project){
+    let box = document.createElement("div");
+    
+    let preview = document.createElement("img");
+    preview.src = `./assets/images/projects/${project.id}/preview.png`;
+    preview.className = "project_img";
+    preview.width = 500;
+    box.appendChild(preview);
+
+    let title = document.createElement("h3");
+    title.innerHTML = `<a href='${project.link}'>${project.title}</a>`;
+    box.appendChild(title);
+
+    let description = document.createElement("p");
+    description.style = "font-size: 16px; width: 80%; margin: auto;";
+    description.innerHTML = project.description;
+    box.appendChild(description);
+
+    let tag_box = document.createElement("ul");
+    tag_box.style = "display: inline-flex;";
+    for (tag in project.tags){
+        let tag_text = project.tags[tag];
+        // Using directly tag of projects.tags changed the size of the image. Yes, that's weird
+        let tagElement = document.createElement("li");
+        tagElement.innerText = `#${tag_text}`;
+        tagElement.style = "margin-right: 30px; list-style-type:none; font-size: 16px;";
+        tag_box.appendChild(tagElement);
+    }
+    box.appendChild(tag_box);
+
+    return box;
+}
